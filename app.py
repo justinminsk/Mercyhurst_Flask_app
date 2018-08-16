1ecdd95c120dad578b27bb64454b6b444b631d11
justin.minsk <justin.minsk@gmail.com>
2018-08-16 14:06:13 -0400

    git init

---
+++ app.py
@@ -0,0 +1,28 @@
+import markdown
+from flask import Flask, render_template, Markup
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/post')
+def content():
+    return render_template('install_python.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route("/slideshow")
+def slide_show():
+    return render_template('slides.html')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ reveal_slides/anaconda/slides.md
@@ -0,0 +1,3 @@
+# Anaconda
+
+Start from here!
